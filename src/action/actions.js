462fef396d511e8ca476e211d111985d62839848
huynh <huynhnguyen120202@gmail.com>
2025-12-23 09:17:25 +0700

    setup redux

---
+++ src/action/actions.js
@@ -0,0 +1,21 @@
+import { INCREMENT, DECREMENT } from './types';
+
+export const increaseCounter = () => {
+
+    return {
+
+        type: INCREMENT,
+
+    };
+
+};
+
+export const decreaseCounter = () => {
+
+    return {
+
+        type: DECREMENT,
+
+    };
+
+};
