c4e617cbd64f199ffd85e6ede207b54a0b8470aa
huynh <huynhnguyen120202@gmail.com>
2025-12-28 22:28:27 +0700

    dispatch and reducer

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
 import { connect } from "react-redux"
-
 import {
   increaseCounter,
   decreaseCounter,
 } from "./action/actions"
+import store from './redux/store';
 
 function App(props) {
+
+  //event handler
+  const handleIncrease = () => {
+    //dispatch actions
+    // props.increaseCounter();
+
+    //fire actions
+    store.dispatch({
+      type: 'test',
+      payload: { 'name': 'huynh'}
+    });
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello world with React!</h1>
         <div>Count: {props.count}</div>
-        <button onClick={() => props.increaseCounter()}>Increase Count</button>
+        <button onClick={() => handleIncrease()}>Increase Count</button>
         <button onClick={() => props.decreaseCounter()}>Decrease Count</button>
       </header>
     </div>
