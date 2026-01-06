469b28697b1394599641f1ebff3edb843fdb4962
huynh <huynhnguyen120202@gmail.com>
2026-01-06 23:00:58 +0700

    Design Layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 import { connect } from "react-redux"
 import {
   increaseCounter,
   decreaseCounter,
 } from "./action/actions"
-import store from './redux/store';
+import axios from 'axios';
 import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import Home from './components/Home';
 
 function App(props) {
   
@@ -17,28 +19,30 @@ function App(props) {
 
   //event handler
   const handleIncrease = () => {
-    //dispatch actions
-    // props.increaseCounter();
-
     dispatch(increaseCounter())
-
-    //fire actions
-    // store.dispatch({
-    //   type: 'test',
-    //   payload: { 'name': 'huynh'}
-    // });
   }
 
+  const fetchAllUser = async () => {
+    const res = await axios.get("http://localhost:8080/users/all");
+    const data = res && res.data ? res.data : []
+    console.log("check data: ", res.data)
+  }
+
+  useEffect(() => {
+    fetchAllUser();
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello world with React and {props.abc}!</h1>
-        <div>Count: {newCount}</div>
-        <button onClick={() => handleIncrease()}>Increase Count</button>
-        <button onClick={() => dispatch(decreaseCounter())}>Decrease Count</button>
-      </header>
-    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <h1>Hello world with React and {props.abc}!</h1>
+    //     <div>Count: {newCount}</div>
+    //     <button onClick={() => handleIncrease()}>Increase Count</button>
+    //     <button onClick={() => dispatch(decreaseCounter())}>Decrease Count</button>
+    //   </header>
+    // </div>
+    <Home />
   );
 }
 
