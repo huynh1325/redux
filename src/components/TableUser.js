4d7bd7b22f26e152cf5d872868572073fa0cb54d
huynh <huynhnguyen120202@gmail.com>
2026-01-08 00:13:01 +0700

    Loading status

--- src/components/TableUser.js
+++ src/components/TableUser.js
@@ -1,28 +1,20 @@
 import Container from 'react-bootstrap/Container';
 import Table from 'react-bootstrap/Table';
-import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { fetchAllUsers } from '../action/actions';
 import { useSelector } from 'react-redux';
 
 const TableUser = (props) => {
 
-    // const [listUsers, setListUsers] = useState();
-
     const dispatch = useDispatch();
-    const listUsers = useSelector((state) => state.user.listUsers );
-
-    // const fetchAllUser = async () => {
-    //     const res = await axios.get("http://localhost:8080/users/all");
-    //     const data = res && res.data ? res.data : []
-    //     setListUsers(data)
-    // }
+    const listUsers = useSelector((state) => state.user.listUsers);
+    const isLoading = useSelector((state) => state.user.isLoading);
+    const isError = useSelector((state) => state.user.isError);
 
     useEffect(() => {
-        // fetchAllUser();
         dispatch(fetchAllUsers());
-    }, [])
+    }, [dispatch])
 
     const handleDeleteUser = (user) => {
         console.log(user)
@@ -42,18 +34,32 @@ const TableUser = (props) => {
                         </tr>
                     </thead>
                     <tbody>
-                        {listUsers && listUsers.length > 0 && listUsers.map((item, index) => {
-                            return (
-                                <tr key={`users-${index}`}>
-                                    <td>{index + 1}</td>
-                                    <td>{item.email}</td>
-                                    <td>{item.username}</td>
-                                    <td>
-                                        <button className='btn btn-danger' onClick={() => handleDeleteUser(item)}>Delete</button>
-                                    </td>
-                                </tr>
-                            )
-                        })}
+                        {isError === true ?
+                            <>
+                                <div>Something wrongs, please try again...</div>
+                            </>
+                            :
+                            <>
+                                {isLoading === true ?
+                                    <><div>Loading data...</div></>
+                                    :
+                                    <>
+                                        {listUsers && listUsers.length > 0 && listUsers.map((item, index) => {
+                                            return (
+                                                <tr key={`users-${index}`}>
+                                                    <td>{index + 1}</td>
+                                                    <td>{item.email}</td>
+                                                    <td>{item.username}</td>
+                                                    <td>
+                                                        <button className='btn btn-danger' onClick={() => handleDeleteUser(item)}>Delete</button>
+                                                    </td>
+                                                </tr>
+                                            )
+                                        })}
+                                    </>
+                                }
+                            </>
+                        }
                     </tbody>
                 </Table>
             </Container>
--- src/reducer/userReducer.js
+++ src/reducer/userReducer.js
@@ -1,8 +1,9 @@
-import { fetchUsersSuccess } from '../action/actions';
 import { FETCH_USER_REQUEST, FETCH_USER_SUCCESS, FETCH_USER_ERROR } from '../action/types';
 
 const INITIAL_STATE = {
-    listUsers: []
+    listUsers: [],
+    isLoading: false,
+    isError: false
 };
 
 const userReducer = (state = INITIAL_STATE, action) => {
@@ -10,17 +11,24 @@ const userReducer = (state = INITIAL_STATE, action) => {
         case FETCH_USER_REQUEST:
             console.log("FETCH_USER_REQUEST: ", action);
             return {
-                ...state
+                ...state,
+                isLoading: true,
+                isError: false
             };
         case FETCH_USER_SUCCESS:
             console.log("FETCH_USER_SUCCESS: ", action);
             return {
-                ...state, listUsers: action.dataUsers
+                ...state,
+                listUsers: action.dataUsers,
+                isLoading: false,
+                isError: false
             };
         case FETCH_USER_ERROR:
             console.log("FETCH_USER_ERROR: ", action);
             return {
-                ...state
+                ...state,
+                isLoading: false,
+                isError: true
             };
         default: return state;
     }
